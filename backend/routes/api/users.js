a927cddeeafb13ba3179d8ed27a7d568baa7e992
Indy Maat <indymaat@gmail.com>
2019-02-25 23:05:04 +0100

    DB opzet

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,79 +1,64 @@
 const express = require('express')
 const router = express.Router()
-
-const users = [
-    {
-        id: 1,
-        name: 'Indy Maat',
-        email: 'i.maat@student.fontys.nl',
-        status: 'active'
-    },
-    {
-        id: 2,
-        name: 'Nils van Eijk',
-        email: 'n.v.eijk@student.fontys.nl',
-        status: 'active'
-    },
-    {
-        id: 3,
-        name: 'Marck Vincent',
-        email: 'm.vincent@student.fontys.nl',
-        status: 'active'
-    }
-]
+const ObjectId = require('mongodb').ObjectId;
 
 // Get all users
 router.get('/', (req, res) => {
-    res.json(users)
+    const collection = req.app.locals.usersCollection
+    collection.find({})
+        .toArray()
+        .then(response => res.status(200)
+        .json(response))
+        .catch(error => console.error(error))
 })
 
 // Get single user
 router.get('/:id', (req, res) => {
-    const found = users.some(user => user.id === parseInt(req.params.id))
-
-    if (found) {
-        res.json(users.filter(user => user.id === parseInt(req.params.id)))
-    } else {
-        res.status(400).json({ 'Bad request': `No user with id: ${req.params.id}`})
-    }
+    const collection = req.app.locals.usersCollection
+    collection.findOne({ _id: parseInt(req.params.id) })
+        .then(response => res.status(200)
+        .json(response))
+        .catch(error => console.error(error))
 })
 
 // Make new user
-router.post('/', (req, res) => {
-    const newuser = {
-        id: users.length + 1,
-        name: req.body.name,
-        email: req.body.email,
-        status: 'active'
-    }
+router.post('/create', (req, res) => {
+    const collection = req.app.locals.usersCollection
 
-    console.log(newuser.name)
-    console.log(newuser.email)
-
-    if (!newuser.name || !newuser.email) {
-        res.status(400).json({ "Missing information": "Please enter a name and email"})
-    }
-    users.push(newuser)
-    res.json(users)
+    // TODO: Rekening houden met counter van _id kolom?
+    collection.insertOne({
+        "name"      : req.body.name,
+        "email"     : req.body.email,
+        "status"    : 'active'
+    }).then(response => res.status(200)
+        .json(response))
+        .catch(error => console.error(error))
 })
 
 // Update user
-router.put('/:id', (req, res) => {
-    const found = users.some(user => user.id === parseInt(req.params.id))
+router.put('/update/:id', (req, res) => {
+    const collection = req.app.locals.usersCollection
+    collection.updateOne(
+        { "_id" : parseInt(req.params.id) },
+        {
+            $set:
+            {
+                "name" : req.body.name,
+                "email": req.body.email
+            },
+            $currentDate: { lastModified: true }
+        }).then(response => res.status(200)
+            .json(response))
+            .catch(error => console.error(error))
+})
 
-    if (found) {
-        const updUser = req.body
-        users.forEach(user => {
-            if (user.id === parseInt(req.params.id)) {
-                user.name = updUser.name ? updUser.name : user.name,
-                user.email = updUser.email ? updUser.email : user.email
-
-                res.json({"User updated": users})
-            }
-        })
-    } else {
-        res.status(400).json({ "User does not exist" : `No user with id: ${req.params.id}`})
-    }
+// Delete user
+router.delete('/delete/:id', (req, res) => {
+    const collection = req.app.locals.usersCollection
+    collection.deleteOne({ "_id" : parseInt(req.params.id) })
+        .then(response => res.status(200)
+        .json(response))
+        .catch(error => console.error(error))
 })
 
 module.exports = router
